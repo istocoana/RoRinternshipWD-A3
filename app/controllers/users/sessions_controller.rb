2e1e1e191772c3eb5c1906a8f759f87e087ef5e6
Oana Istoc <o.istoc@outlook.com>
2023-08-08 13:33:11 +0300

    Register and login functionalities

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Users::SessionsController < Devise::SessionsController
   skip_before_action :verify_authenticity_token
 
@@ -10,4 +11,11 @@ class Users::SessionsController < Devise::SessionsController
 
   end
 
+=======
+class SessionsController < ApplicationController
+  def destroy
+    sign_out(current_user) if user_signed_in?
+    redirect_to root_path, notice: 'You have been signed out successfully.'
+  end
+>>>>>>> a48ecfb (Sign out problem)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,18 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
->>>>>>> 83994c0 (Register and login functionalities)
   root "home#index"
 
   get "/about", to: "static_pages#about", as: :about
   get "/blog", to: "static_pages#blog"
   get "/contacts", to: "static_pages#contacts"
   get "/services", to: "static_pages#services"
-  
   get "/product", to: "products#product"
-
   get '/services/product_filter_service', to: 'product_filter_service#filter'
-
-
   resources :products, only: [:index, :show]
 
-<<<<<<< HEAD
-=======
   resources :profiles, only: [:new, :create, :edit, :update]
   get 'profile', to: 'profiles#show', as: :user_profile
   get 'profile_updated', to: 'profiles#updated', as: :profile_updated
 
->>>>>>> 83994c0 (Register and login functionalities)
 
 end
