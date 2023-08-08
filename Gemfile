fade22538968ee2646e0fab24fd258213998b2a7
Oana Istoc <o.istoc@outlook.com>
2023-08-08 09:32:49 +0300

    Migrate to the database

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.2.2"
 
+gem 'devise', '~> 4.9', '>= 4.9.2'
 
 gem "image_processing", '~> 1.2'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.active_storage.variant_processor = :mini_magick
 
