169a880979c0d94e0b64501a6192823b355398db
Oana Istoc <o.istoc@outlook.com>
2023-08-09 14:43:57 +0300

    Cherry pick

--- app/javascript/category-filter.js
+++ app/javascript/category-filter.js
@@ -91,3 +91,4 @@ function applyFilters(){
   form.submit();
   
 }
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   root "home#index"
+
   get "/about", to: "static_pages#about", as: :about
   get "/blog", to: "static_pages#blog"
   get "/contacts", to: "static_pages#contacts"
   get "/services", to: "static_pages#services"
   get "/product", to: "products#product"
   get '/services/product_filter_service', to: 'product_filter_service#filter'
+
   resources :products, only: [:index, :show]
   resources :profiles, only: [:new, :create, :edit, :update]
   get 'profile', to: 'profiles#show', as: :user_profile
   get 'profile_updated', to: 'profiles#updated', as: :profile_updated
+
 
 end
