6400870e6968fdd2d887123d4bc10bd1db493e6f
Oana Istoc <o.istoc@outlook.com>
2023-08-17 08:29:36 +0300

    Change position of toastr notifications

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,11 @@
 $(document).ready(function() {
   var notice = $('.notice').data('notice');
   var alert = $('.alert').data('alert');
+
+  toastr.options = {
+    positionClass: 'toast-top-left',
+  };
+  
   if (notice) {
     toastr.success(notice);
   } else if (alert) {
